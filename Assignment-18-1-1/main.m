dd594b88fa534118476acdea67e321597a4129de
Yonas B Haile <yhaile@md3612-38178-15.matc.madison.login>
2013-10-21 13:49:54 -0500

    Initial Commit

---
+++ Assignment-18-1-1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Assignment-18-1-1
+//
+//  Created by Yonas B Haile on 10/21/13.
+//  Copyright (c) 2013 Yonas B Haile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
